c85d52c5694bf120d4e98889ebbe1f1513ad67b2
Júlio <julio.jeronimo@somagrupo.com.br>
2021-11-04 20:28:46 -0300

    Creating menu

---
+++ src/components/menu/menu.style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const MenuContainer = styled.div((props)=>({
+    backgroundColor: '#03045E',
+    height: '100vh',
+    width: 120
+}))
+
+
+export {MenuContainer}
