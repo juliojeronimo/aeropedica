a5e23a312abf242d2f9e83c49b208eaf61b98adb
Júlio <julio.jeronimo@somagrupo.com.br>
2021-11-09 01:01:27 -0300

    First menu done

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,8 +1,18 @@
-import { MenuContainer } from './menu.style';
-const Menu = ({ menuItems}) => {
+import { LogoContainer, MenuContainer, MenuItem } from './menu.style';
+import logo from '../../assets/images/logo-default.png'
+
+let menuList = ['Aeroportos', 'Aeronaves', 'Companhias', 'Equipamentos', 'Passageiros', 'Reservas', 'Rotas de voo', 'Voos', 'UF', 'País']
+
+const Menu = ({ menuItems, pageIndex}) => {
 
     return (
       <MenuContainer>
+          <LogoContainer><img style={{maxHeight: 100, width: 80}} src={logo}/></LogoContainer>
+          
+          {menuList.map((item,index)=>{
+              return <MenuItem onClick={()=> console.log('crica ai')} selected={index===pageIndex}>{item}</MenuItem>
+          })} 
+        
 
       </MenuContainer>
     );
