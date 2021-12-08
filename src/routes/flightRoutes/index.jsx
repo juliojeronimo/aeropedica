242521f1f4d85f77945dae02cc145e1437e19fe0
Ismael <ismaelhugo.dev@gmail.com>
2021-12-07 22:02:06 -0300

    Listar Rotas

--- src/routes/flightRoutes/index.jsx
+++ src/routes/flightRoutes/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Menu } from '../../components/menu'
 import { Table } from '../../components/table'
 import {Button} from '../../components/button'
@@ -7,160 +7,18 @@ import instance from '../../services/instance'
 
 let HeaderList = ['Cód. Origem', 'Cód. Destino', 'Num. Rota', 'Voucher',  'Actions']
 
-let objects = [
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-    {
-        'cod_airport': '1',
-        'cod_equip': '0378',
-        'cod_companie': 'Aero/LATAM',
-        'cod_voucher': 'EA2F552'
-    },
-]
-
 const FlightRoutes = ({ }) => {
+
+    const [routes, setRoutes] = useState([])
+
+    useEffect(()=> {
+        instance.get('./api/Rota/Listar').then((res) => {
+            console.log(JSON.stringify(res))
+            setRoutes(res.data)
+        }).catch((e) => {
+            console.log(JSON.stringify(e))
+        })
+    })
 
     return (
         <Container>
@@ -169,7 +27,7 @@ const FlightRoutes = ({ }) => {
                 <Title>Rotas de voo</Title>
                 <Button style={{margin: 50}} label={'Cadastrar'} variant={'primary'} size={'extra-large'}/>
                 <Table header={HeaderList}
-                    list={objects}
+                    list={routes}
                     updateItem={() => console.log('editando')}
                     deleteItem={() => console.log('deletando')} />
             </SideBox>
